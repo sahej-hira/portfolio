ec74ca911ada59cad159b736e89eb7e049025340
Sahej <sahej.k.hira@gmail.com>
2024-10-26 18:21:56 +0530

    resume updated

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -42,7 +42,7 @@ const SocialLinks = () => {
                         Resume <BsFillPersonLinesFill size={30}/>
                     </>
                 ),
-                href: './resume (3).pdf',
+                href: './RESUME (5).pdf',
                 style: 'rounded-br-md',
                 download: true,
             },
